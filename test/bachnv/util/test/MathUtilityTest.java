021324c9f5a5b97afb515d9f406c1899330a3f2c
ngvbach2000 <ngvbach2000@gmail.com>
2020-11-17 15:50:51 +0700

    add Test

---
+++ test/bachnv/util/test/MathUtilityTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bachnv.util.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ngvba
+ */
+public class MathUtilityTest {
+    
+    public MathUtilityTest() {
+    }
+    
+}
