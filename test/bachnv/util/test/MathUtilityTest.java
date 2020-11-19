78df97e1634d80d122c0d442945f636d5aac0eb3
ngvbach2000 <ngvbach2000@gmail.com>
2020-11-19 13:52:10 +0700

    add test

--- test/bachnv/util/test/MathUtilityTest.java
+++ test/bachnv/util/test/MathUtilityTest.java
@@ -5,6 +5,7 @@
  */
 package bachnv.util.test;
 
+import bachnv.util.MathUtility;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -14,7 +15,25 @@ import static org.junit.Assert.*;
  */
 public class MathUtilityTest {
     
-    public MathUtilityTest() {
-    }
+    @Test // ki hieju cua Junit dua ra, bien ham sau day thanh psvm, app co nhieu main
+    //F6 chay main mac dinh, Shift F6 la chay main ow @Test
     
+    //dat ten ham test de test code chinh
+    //test thang nao_trang thai gi_ ...
+    public void getFactorial_RunsWell_IfValidArgument() {
+        assertEquals(720, MathUtility.getFactorial(6));
+        assertEquals(120, MathUtility.getFactorial(5));
+        assertEquals(24, MathUtility.getFactorial(4));
+        assertEquals(6, MathUtility.getFactorial(3));
+        assertEquals(10, MathUtility.getFactorial(0));
+    }
+    //xanh chi dat duoc neu tat ca cung xanh, dung het moi tinh huong
+    //do chi can it nhat mot thang do, chap bat car thanh phan con lai
+    //ham ma da dung thi dung het, sai 1 cai hay vai cai ->ham k dang tin
 }
+
+//viet code de test code chinh
+//viet code dung thu vie Jnit, xUnit, Jasmine
+//xem code chinh chajy on khong truong khi dem qua Qc, KH
+//bao dung sai qua mau sac xanh, do
+//mat nhin 2 mau xanh do la du, khoi can so sanh ket qua
